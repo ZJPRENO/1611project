5cde22ec755ea2ba796c4555db3e115f14f6287e
ZJPRENO <626565920@qq.comgit config --global user.email 626565920@qq.com>
2016-12-06 09:52:08 +0800

    初始化提交

---
+++ github/github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by 张俊平 on 16/12/6.
+//  Copyright © 2016年 张俊平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/github/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  github
+//
+//  Created by 张俊平 on 16/12/6.
+//  Copyright © 2016年 张俊平. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
